e769867405db8a5d6f75d8dd05cf6ec4218b774a
rosaliawk <rosalia.wong@gmail.com>
2015-12-28 01:29:40 -0800

    Update input_listings.py

--- input_listings.py
+++ input_listings.py
@@ -8,7 +8,11 @@ from question2 import get_stats
 
 app = Flask(__name__)
 
-@app.route('/dataset/common_stats/',methods=['POST'])
+@app.route('/')
+def hello():
+    return 'For common stats, send request to /dataset/common_stats/ with city, state and beds specfied\n'
+
+@app.route('/dataset/common_stats/',methods=['GET','POST'])
 def get_common_stats():
     if request.headers['Content-Type'] == 'application/json':
         try:
@@ -17,19 +21,27 @@ def get_common_stats():
             beds = request.get_json(force=True)['beds']
             return get_stats(city, state, beds)            
         except KeyError as e:
+            print ('Missing %s in input\n'%str(e))
             return ('Missing %s in input\n'%str(e))
-        except:
+        except TypeError as e:
+            print ('Get common stats failed: %s\n'%str(e))
             return ('Get common stats failed: %s\n'%str(e))
+        except ValueError as e:
+            print ('Get common stats failed: %s\n'%str(e))
+            return ('Get common stats failed: %s\n'%str(e))
+        except:
+            print ('Get common stats failed\n')
+            return ('Get common stats failed\n')
     else:
-        return "Only json content-type accepted"
+        print ("Only json content-type accepted\n")
+        return "Only json content-type accepted\n"
 
 
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     if (len(sys.argv)>1):
         get_listing(*(sys.argv[1:])) 
     else:
-        app.run()
+        app.run(host='0.0.0.0') 
     
